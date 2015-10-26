8c62a2c6ada169e3a2e26c86d9902edd06d52770
Nicholas <Nicholas@10.30.19.68>
2015-10-26 17:17:15 -0500

    Finished

--- src/dateutilities/DateUtil.java
+++ src/dateutilities/DateUtil.java
@@ -1,3 +1,5 @@
+package dateutilities;
+
 
 import java.text.DateFormat;
 import java.time.LocalDate;
@@ -15,28 +17,57 @@ import javax.swing.text.DateFormatter;
  *
  * @author Nicholas
  */
+/**
+ * different utilities for dates
+ * @author Nicholas
+ */
 public class DateUtil {
+    /**
+     * takes a local date time variable and converts to string
+     * @param date local date time 
+     * @return 
+     */
    public String toString(LocalDateTime date) {
         DateTimeFormatter df = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
         return df.format(date);
     }
+   /**
+    * takes a local date and converts to string
+    * @param d local date
+    * @return 
+    */
    public String toString(LocalDate d){
        DateTimeFormatter df = DateTimeFormatter.ISO_LOCAL_DATE;
        return df.format(d);
    }
+   /**
+    * takes a string and converts to local date variable
+    * @param s string representing a date in format yyyy-MM-dd
+    * @return 
+    */
    public LocalDate toDate(String s){
        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
        LocalDate date = LocalDate.parse(s, df);
        return date;
    }
+   /**
+    * takes a string and converts to local date time
+    * @param date takes a string representing a date and time in format yyyy-MM-dd HH:mm
+    * @return 
+    */
    public LocalDateTime toDateTime(String date){
        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
        LocalDateTime date2 = LocalDateTime.parse(date, df);
        return date2;
    }
-   
-   public LocalDate addDays(LocalDate jim,long daysToAdd){
-       return jim.plusDays(daysToAdd);
+   /**
+    * takes a localdate variable and a long number and adds that number for days to the local date
+    * @param lc local date
+    * @param daysToAdd long representing days
+    * @return 
+    */
+   public LocalDate addDays(LocalDate lc,long daysToAdd){
+       return lc.plusDays(daysToAdd);
    }
    
     public static void main(String[] args) {
